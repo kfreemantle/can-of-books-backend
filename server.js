24930755abc94bd23a9e14422cee9d52bcd7beef
Kyle Freemantle <kcfreemantle@gmail.com>
2023-03-13 15:09:39 -0700

    added proof of life backend, I hope

--- server.js
+++ server.js
@@ -4,7 +4,22 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+
+// connecting the DB via mongoose
+// add validation to confirm we are wired up to our mongo DB
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+// connecting Mongoose to our MongoDB instance
+mongoose.connect(process.env.MONGO_DB_URL);
+
+
+// EXPRESS invocation here
 const app = express();
+// CORS invocation here
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
